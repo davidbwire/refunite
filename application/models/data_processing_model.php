14ad57e602e4f3c139ad6bb3fc2efd117a6a32c4
israel bwire <israelbwire@gmail.com>
2013-06-16 15:41:22 +0300

    Created the data processing model

---
+++ application/models/data_processing_model.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Description of data_processing_model
+ *
+ * @author david bwire
+ */
+class data_processing_model extends CI_Model {
+    const LOST_PEOPLE = '';
+
+
+    /*
+     * @description group members searching lost persons from the same lastSighting
+     * @returrn array
+     * 
+     */
+
+    public function get_people_searching_from_common_last_sighting() {
+        
+    }
+
+}
+
+?>
